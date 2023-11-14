229af680fe3afb90beb7b01afd395b4dce94ccb1
Florian Lauch <florian.lauch@epitech.eu>
2023-11-14 16:01:33 +0100

    feat(algo): Random game simulation

---
+++ src/game/random.rs
@@ -0,0 +1,50 @@
+use crate::game::GameEnd;
+use crate::handler::GameHandler;
+use rand::{thread_rng, Rng};
+
+impl GameHandler {
+    pub fn simulate_random_game(&mut self, mut turn: bool) -> GameEnd {
+        let mut winning = false;
+        let mut pos: (i8, i8);
+        let mut turns: i32 = self.turns;
+
+        while !winning {
+            if turns == self.max_turns {
+                return GameEnd::Draw;
+            }
+            pos = self.get_random_move();
+            self.table[pos.0 as usize][pos.1 as usize] = if turn { 1 } else { 2 };
+            turns += 1;
+            turn = !turn;
+            winning = self.is_move_winning(pos);
+        }
+        self.restore_table();
+        if turn {
+            GameEnd::Defeat
+        } else {
+            GameEnd::Victory
+        }
+    }
+
+    fn get_random_move(&self) -> (i8, i8) {
+        let mut random = thread_rng();
+        let mut x;
+        let mut y;
+
+        loop {
+            x = random.gen_range(0..self.size.0);
+            y = random.gen_range(0..self.size.1);
+            if self.table[x as usize][y as usize] == 0 {
+                return (x, y);
+            }
+        }
+    }
+
+    fn restore_table(&mut self) {
+        for x in 0..self.size.0 as usize {
+            for y in 0..self.size.1 as usize {
+                self.table[x][y] = self.state[x][y];
+            }
+        }
+    }
+}
